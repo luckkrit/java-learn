5397f04784efbceb0720fe1e0e98f18ba77b1612
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 01:01:18 +0700

    Init Project Create Lab1 and Lab2

---
+++ ch1_9/lab1/Lab1.java
@@ -0,0 +1,7 @@
+package ch1_9.lab1;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ ch1_9/lab2/Lab2.java
@@ -0,0 +1,7 @@
+package ch1_9.lab2;
+
+public class Lab2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
