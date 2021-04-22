a05bcc7262ce8c9f1e4e6329b68864ecb22e8d13
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 08:32:34 +0700

    Finish Lab5

---
+++ ch1_9/lab5/Lab5.java
@@ -0,0 +1,27 @@
+package ch1_9.lab5;
+
+public class Lab5 {
+    public static void main(String[] args) {
+        int score = 100;
+        switch(score){
+            case 80:
+                System.out.println("You got A");
+                break;
+            case 70:
+                System.out.println("You got B");
+                break;
+            case 60:
+                System.out.println("You got C");
+                break;
+            case 50:
+                System.out.println("You got D");
+                break;
+            case 40:
+                System.out.println("You got F");
+                break;
+            default:
+                System.out.println("You got E");
+                break;
+        }
+    }
+}
