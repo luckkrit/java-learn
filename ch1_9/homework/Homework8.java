01d9d5c4f4e601e3d17e2edb47d33fb24e079593
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 13:17:18 +0700

    Add Homework and group Lab6 - Lab8 together

---
+++ ch1_9/homework/Homework8.java
@@ -0,0 +1,5 @@
+package ch1_9.homework;
+
+public class Homework8 {
+
+}
---
+++ ch1_9/lab6_8/Lab6_8.java
@@ -0,0 +1,16 @@
+package ch1_9.lab6_8;
+
+import ch1_9.lab6.Lab6;
+import ch1_9.lab7.Lab7;
+import ch1_9.lab8.Lab8;
+
+public class Lab6_8 {
+    public static void main(String[] args) {
+        // Lab 6
+        Lab6.main(args);
+        // Lab 7
+        Lab7.main(args);
+        // Lab 8
+        Lab8.main(args);
+    }
+}
