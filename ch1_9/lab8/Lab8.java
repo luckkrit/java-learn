95bf905e6d553b9e07f98bcac0e908296964aab0
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 08:50:08 +0700

    Finish Lab8

---
+++ ch1_9/lab8/Lab8.java
@@ -0,0 +1,32 @@
+package ch1_9.lab8;
+
+public class Lab8 {
+    public static void main(String[] args) {
+        // 1.a
+        int count = 0;
+        while (count <= 20) {
+            System.out.println("count = " + count);
+            count++;
+        }
+        // 1.b
+        count = 0;
+        while (count <= 20) {
+            System.out.println("count = " + count);
+            if (count == 11) {
+                break;
+            }
+            count++;
+        }
+        // 1.c
+        count = 0;
+        while (count <= 20) {
+            if (count == 11) {
+                System.out.println("count = " + count);
+                break;
+            } else {
+                System.out.println("count = " + count);
+            }
+            count++;
+        }
+    }
+}
