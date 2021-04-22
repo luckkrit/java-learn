4c4034b0c5b199f97c57950ae71331a17fb61386
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 09:52:29 +0700

    Finish Lab13

---
+++ ch1_9/lab13/Lab13.java
@@ -0,0 +1,25 @@
+package ch1_9.lab13;
+
+public class Lab13 {
+    public static void main(String[] args) {
+        // 1
+        int[][] d2 = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int sumLastColumn = 0;
+        for (int[] row : d2) {
+            for (int j = 0; j < row.length; j++) {
+                System.out.print(row[j] + " ");
+                if (j == row.length - 1) {
+                    sumLastColumn += row[j];
+                }
+            }
+            System.out.println();
+        }
+        // 2
+        System.out.println("Sum of last column from 2D array = " + sumLastColumn);
+
+    }
+}
