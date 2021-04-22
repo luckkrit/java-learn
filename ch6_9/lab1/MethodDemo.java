389a5b8c2388224345c99ce52eae754eb45e57e3
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 15:11:00 +0700

    Finish Lab1

--- ch6_9/lab1/MethodDemo.java
+++ ch6_9/lab1/MethodDemo.java
@@ -18,10 +18,33 @@ public class MethodDemo {
         int ans5 = addAndSubtract(3, 4, 1);
         System.out.println(ans5);
         String ans6 = add("123", "456");
+        System.out.println(ans6);
+        int ans7 = multiply(2, 10);
+        System.out.println(ans7);
+        int ans8 = 0;
+        try {
+            ans8 = divideBy(ans7, 2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(ans8);
+
+        specialBonus = 100;
+        ans = add(3, 4);
+        System.out.println(ans);
+        ans2 = add(ans, 2);
+        System.out.println(ans2);
+        ans3 = add(ans2, 10, 10);
+        System.out.println(ans3);
+        ans4 = subtract(ans3, 3);
+        System.out.println(ans4);
+        ans5 = addAndSubtract(3, 4, 1);
+        System.out.println(ans5);
+
     }
 
     public static int add(int a, int b) {
-        int answer = a + b;
+        int answer = (a + b) * 2;
 
         return specialBonus + answer;
     }
@@ -40,6 +63,32 @@ public class MethodDemo {
     public static int subtract(int a, int b) {
         int answer = a - b;
 
+        return specialBonus + answer;
+    }
+
+    /**
+     * Multiply number
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int multiply(int a, int b) {
+        int answer = a * b;
+        return specialBonus + answer;
+    }
+
+    /**
+     * Divide number
+     *
+     * @param a
+     * @param b
+     * @return
+     * @throws Exception
+     */
+    public static int divideBy(int a, int b) throws Exception {
+        if (b == 0) throw new Exception("b is zero");
+        int answer = a / b;
         return specialBonus + answer;
     }
 
