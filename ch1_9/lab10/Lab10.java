e858a77da9a1708bf201664cf4f5f215dbb2343c
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-22 08:56:17 +0700

    Finish Lab10

---
+++ ch1_9/lab10/Lab10.java
@@ -0,0 +1,13 @@
+package ch1_9.lab10;
+
+public class Lab10 {
+    public static void main(String[] args) {
+        for (int count = 1; count <= 10; count++) {
+            if (count == 2) continue;
+            else {
+                System.out.println("count = " + count);
+            }
+
+        }
+    }
+}
